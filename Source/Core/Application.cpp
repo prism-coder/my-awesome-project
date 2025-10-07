f57ab2bcc4be9a0010c29d1d4fbec879547fde3d
Prism | Software Developer <prism27.dev@gmail.com>
2025-10-07 16:57:47 +0200

    style: :lipstick: Changed the line breaks of the Application.cpp file
    Changed the line breaks of the Application.cpp file

--- Source/Core/Application.cpp
+++ Source/Core/Application.cpp
@@ -10,38 +10,46 @@ Application::Application(const ApplicationSpecification& specification)
 	Initialize();
 }
 
-Application::~Application() {
+Application::~Application()
+{
 	Shutdown();
 }
 
-void Application::Run() {
-	while (m_Running) {
+void Application::Run()
+{
+	while (m_Running)
+	{
 		auto now = std::chrono::high_resolution_clock::now();
 		float time = std::chrono::duration<float>(now.time_since_epoch()).count();
 		Timestep timestep = time - m_LastTime;
 		m_LastTime = time;
 
-		for (Ref<Layer>& layer : m_Layers) {
+		for (Ref<Layer>& layer : m_Layers)
+		{
 			layer->OnUpdate(timestep);
 		}
 	}
 }
 
-void Application::PushLayer(Ref<Layer> layer) {
+void Application::PushLayer(Ref<Layer> layer)
+{
 	std::cout << "New Layer is being pushed" << std::endl;
 
 	m_Layers.push_back(layer);
 	layer->OnAttach();
 }
 
-void Application::Quit() {
+void Application::Quit()
+{
 	m_Running = false;
 }
 
-void Application::Initialize() {
+void Application::Initialize()
+{
 	std::cout << "Application initialized" << std::endl;
 }
 
-void Application::Shutdown() {
+void Application::Shutdown()
+{
 	std::cout << "Application shut down" << std::endl;
 }
